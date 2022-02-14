691ea0f0b64a5eb339d5d753c3cdb572748b6aca
Javohir <narzullayevj999@gmail.com>
2022-02-14 14:48:21 +0500

    multi consumer send by key by directly

--- src/main/java/directly/multiconsumer/Producer.java
+++ src/main/java/directly/multiconsumer/Producer.java
@@ -16,6 +16,7 @@ public class Producer {
 
     private final static String QUEUE_NAME = "test_qd1";
     private final static String key = "color.green";
+    private final static String key2 = "color.blue";
     private final static String DirectExchange="java_directly";
 
     public static void main(String[] args) throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
@@ -25,14 +26,22 @@ public class Producer {
             Channel channel = connection.createChannel()) {
             int count=0;
             do {
-                String message = "Hello World From Java!---->"+count;
+                String message = "For queue it key color green Java!---->"+count;
 
 
                 channel.basicPublish(DirectExchange, key, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
                 System.out.println(" [x] Sent '" + message + "'");
                 ++count;
                  Thread.sleep(4_00);
-            }while (true);
+            }while (count<10);
+
+            do {
+                String message = "For queue it key color blue Java!---->"+count;
+                channel.basicPublish(DirectExchange, key2, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+                ++count;
+                Thread.sleep(4_00);
+            }while (count<21);
 
         } catch (IOException | TimeoutException | InterruptedException e) {
             throw new RuntimeException(e);
